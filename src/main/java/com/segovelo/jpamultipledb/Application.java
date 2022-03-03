9828100b0995c07ee9ffe18282b444cde35c9f27
segovelo <gauchosinpingo@gmail.com>
2022-03-03 22:51:00 +0000

    Rename to Application file

--- src/main/java/com/segovelo/jpamultipledb/Application.java
+++ src/main/java/com/segovelo/jpamultipledb/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JpaMultipleDbApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JpaMultipleDbApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
--- src/test/java/com/segovelo/jpamultipledb/ApplicationTests.java
+++ src/test/java/com/segovelo/jpamultipledb/ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class JpaMultipleDbApplicationTests {
+class ApplicationTests {
 
 	@Test
 	void contextLoads() {
