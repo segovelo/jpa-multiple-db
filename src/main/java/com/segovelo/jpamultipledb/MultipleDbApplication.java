c09de00d2d687e5da574823ca6d0aa0a883a723e
segovelo <gauchosinpingo@gmail.com>
2022-03-03 23:21:13 +0000

    added MultipleDbApplication.

---
+++ src/main/java/com/segovelo/jpamultipledb/MultipleDbApplication.java
@@ -0,0 +1,17 @@
+package com.segovelo.jpamultipledb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/** 
+* 3 Mar 2022 23:19:46
+* @Javadoc TODO 
+*
+* @author Sebastian Vergara Losada  **/
+@SpringBootApplication
+public class MultipleDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultipleDbApplication.class, args);
+    }
+    
+}
