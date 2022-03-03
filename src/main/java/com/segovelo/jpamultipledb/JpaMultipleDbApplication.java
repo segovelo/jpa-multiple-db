9657b5ce5d2a140afc0023ca0352e2d3e2ea914b
segovelo <gauchosinpingo@gmail.com>
2022-03-03 18:54:10 +0000

    Initial commit

---
+++ src/main/java/com/segovelo/jpamultipledb/JpaMultipleDbApplication.java
@@ -0,0 +1,13 @@
+package com.segovelo.jpamultipledb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaMultipleDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaMultipleDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/segovelo/jpamultipledb/JpaMultipleDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.segovelo.jpamultipledb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaMultipleDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
