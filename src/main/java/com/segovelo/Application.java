890ce50e526c6199575f743726bb97fd27f63477
segovelo <gauchosinpingo@gmail.com>
2022-03-03 23:39:22 +0000

    Added Application file.

---
+++ src/main/java/com/segovelo/Application.java
@@ -0,0 +1,19 @@
+package com.segovelo;
+
+/** 
+* 3 Mar 2022 23:38:24
+* @Javadoc TODO 
+*
+* @author Sebastian Vergara Losada  **/
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
